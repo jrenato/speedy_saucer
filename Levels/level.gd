80c969e49124bda4123d10c3a3cc4e661131bb09
karvalho <renato@karvalho.com.br>
2023-06-10 11:49:24 -0300

    Added win condition

--- Levels/level.gd
+++ Levels/level.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+@onready var win_label = %WinLabel
+@onready var player = %Player
+
+
+func _ready():
+	win_label.visible = false
+
 
 func _on_maze_body_exited(body):
 	get_tree().reload_current_scene()
+
+
+func _on_win_area_body_entered(body):
+	win_label.visible = true
+	player.linear_velocity = Vector2.ZERO
+	player.won = true
--- Player/player.gd
+++ Player/player.gd
@@ -1,9 +1,13 @@
 extends RigidBody2D
 
-var force = 1500
+var force : int = 1500
+var won : bool = false
 
 
-func _physics_process(delta):
+func _physics_process(delta : float):
+	if won:
+		return
+
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
 	if Input.is_action_pressed("move_left"):
